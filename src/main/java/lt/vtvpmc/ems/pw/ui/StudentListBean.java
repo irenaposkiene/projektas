55e95dc3b0c91a8de72d10700f665ccf3f63c94c
irenaposkiene <iren.poskiene@gmail.com>
2017-03-25 14:01:39 +0200

    template

---
+++ src/main/java/lt/vtvpmc/ems/pw/ui/StudentListBean.java
@@ -0,0 +1,28 @@
+
+package lt.vtvpmc.ems.pw.ui;
+
+import lt.vtvpmc.ems.pw.entities.Student;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.List;
+
+public class StudentListBean {
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Transactional(readOnly = true)
+    public List<?> getStudentList() {
+        Query query = entityManager.createQuery("select c from Student c");
+        return query.getResultList();
+    }
+    
+    @Transactional
+    public void removeStudent(Student student) {
+        entityManager.remove(entityManager.merge(student));
+    }
+    
+}
